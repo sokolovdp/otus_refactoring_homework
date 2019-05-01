ff737ffa8cf995753a1a249bcd2dd201ddadae0c
dmitrii <sokolovdp@gmail.com>
2019-05-01 14:51:38 +0300

    pep8 correction

--- data_out.py
+++ data_out.py
@@ -3,7 +3,7 @@ from collections import Counter
 TOP_VERBS_AMOUNT = 10
 
 
-def print_results(results: list, top_verbs_amount=TOP_VERBS_AMOUNT):
+def print_results(results: list, top_verbs_amount: int = TOP_VERBS_AMOUNT):
     for result in results:
         if result.verbs:
             top_verbs = Counter(result.verbs).most_common(top_verbs_amount)
