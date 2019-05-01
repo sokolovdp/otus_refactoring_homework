7e127a44302ef7eeda1f4d56d0cce8c6cae819e4
dmitrii <sokolovdp@gmail.com>
2019-05-01 20:46:11 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sokolovdp/otus_refactoring_homework",
+    py_modules={'code_parser', 'data_out'},
     packages=setuptools.find_packages(),
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3.7.3",
         "License :: OSI Approved :: MIT License",
